7512e8d465efbeb1bfdad93bd35b38a8e849e3e4
jcbowen <bowen@jiuchet.com>
2023-11-17 12:01:12 +0800

    完成tcp重构

--- src/tcp/console/components/Server.php
+++ src/tcp/console/components/Server.php
@@ -2,6 +2,7 @@
 
 namespace Jcbowen\yiiswoole\tcp\console\components;
 
+use Jcbowen\JcbaseYii2\components\ErrCode;
 use Jcbowen\yiiswoole\components\Context;
 use Jcbowen\yiiswoole\components\Util;
 use Swoole\Process;
@@ -9,118 +10,108 @@ use Swoole\Server\Port;
 use Swoole\Table;
 use Swoole\Server as SwServer;
 use Yii;
+use yii\base\Component;
+use yii\base\InvalidArgumentException;
 use yii\base\InvalidRouteException;
+use yii\console\Controller;
 use yii\console\Exception;
 use yii\console\Response;
 use yii\helpers\ArrayHelper;
+use yii\helpers\BaseConsole;
 
 /**
- * TCP Server
+ * Class Server
  *
+ * @descripton server for tcp
  * @author Bowen
  * @email bowen@jiuchet.com
- * @lasttime: 2022/8/15 2:15 PM
+ * @lasttime: 2023/11/16 2:47 PM
  * @package Jcbowen\yiiswoole\tcp\console\components
  */
-class Server
+class Server extends Component
 {
-    /** @descripton 服务配置 */
-    protected $serverConfig;
-
-    /** @descripton 需要监听的端口 */
-    protected $serverPorts;
-
-    /** @descripton 替换server的监听回调 */
-    protected $onServer;
-
-    /** @descripton 需要创建的内存级table */
-    protected $tablesConfig;
+    // -----
+    /**
+     * @var array 标准的swoole4配置项都可以在此加入
+     */
+    protected array $serverConfig;
 
     /**
-     * @var array
+     * @var array 监听多端口时，每个端口的配置信息
      */
-    public $_tables = [];
+    protected array $serverPorts;
 
     /**
-     * 主服务
-     * @var SwServer
+     * @descripton 需要创建的内存级table们
      */
-    public $_ms = [];
+    protected array $tablesConfig;
+
+    /** @var Controller console控制器 */
+    protected Controller $Controller;
+
+    // -----
+    /**
+     * @var array 实例化的内存级table都会存储在此
+     */
+    public array $_tables = [];
+
+    /**
+     * @var SwServer|null tcp实例
+     */
+    public ?SwServer $_ms = null;
 
     /**
      * pid 文件所在位置
      * @var string
      */
-    public $pidFile = '';
+    public string $pidFile = '';
 
     /**
      * 所有监听成功的端口
      * @var array
      */
-    public $ports = [];
+    public array $ports = [];
 
     /**
-     * Server constructor.
-     * @param array $serverConfig 通用配置
-     * @param array $serverPorts 多端口配置
-     * @param $onServer
-     * @param array $tablesConfig
+     * {@inheritdoc}
      */
-    public function __construct(array $serverConfig, array $serverPorts, $onServer = null, array $tablesConfig = [])
+    public function init()
     {
-        $this->serverConfig = $serverConfig;
+        if (empty($this->serverConfig))
+            throw new InvalidArgumentException('Configuration is empty. Please check the configuration file.');
 
-        $this->serverPorts = $serverPorts;
-
-        $this->onServer = $onServer ?: null;
-
-        $this->tablesConfig = $tablesConfig;
-
-        if (empty($this->serverConfig)) {
-            echo '配置信息为空，请检查配置文件' . PHP_EOL;
-            die();
-        }
-
-        if (!empty($this->serverConfig['pid_file'])) {
-            $this->pidFile = $this->serverConfig['pid_file'];
-        }
-
-        if (empty($this->pidFile)) {
-            echo '请设置pid_file路径' . PHP_EOL;
-            die();
-        }
+        $this->pidFile = $this->serverConfig['pid_file'] ?: '@runtime/yiiswoole/websocket.pid';
     }
 
     /**
-     * 运行服务
+     * 运行tcp服务
      *
      * @author Bowen
      * @email bowen@jiuchet.com
      *
      * @return SwServer
-     * @lasttime: 2022/8/5 2:55 PM
+     * @lasttime: 2023/11/16 2:48 PM
      */
     public function run(): SwServer
     {
-        // 根据配置文件创建内存共享table
-        if (!empty($this->tablesConfig) && is_array($this->tablesConfig)) {
+        // 如果在配置文件中，配置了内存共享table，则创建
+        if (!empty($this->tablesConfig)) {
             foreach ($this->tablesConfig as $ind => $tableConfig) {
                 if (empty($tableConfig['column']) || !is_array($tableConfig['column'])) continue;
                 $tableSize           = $tableConfig['size'] ?: 1024;
                 $conflict_proportion = $tableConfig['conflict_proportion'] ?: 0.2;
-                /** @var Table */
                 $this->_tables[$ind] = new Table($tableSize, $conflict_proportion);
 
-                foreach ($tableConfig['column'] as $column) {
+                foreach ($tableConfig['column'] as $column)
                     $this->_tables[$ind]->column($column['name'], $column['type'], $column['size']);
-                }
+
                 $this->_tables[$ind]->create();
             }
         }
 
         static $first = '';
         foreach ($this->serverPorts as $k => $port) {
-            // 将第一个作为主服务
+            // 将第一个端口作为主服务
             if (empty($first)) {
                 $this->_ms = new SwServer($port['host'], $port['port'], $port['mode'], $port['socketType']);
 
@@ -130,9 +121,9 @@ class Server
                 // 将配置中的地址为swoole能理解的绝对地址
                 Util::translateArrayFilePath($portConfig);
 
-                if (empty($portConfig['cert'])) {
+                // 如果不需要证书，则清理掉相关参数
+                if (empty($portConfig['cert']))
                     unset($portConfig['ssl_cert_file'], $portConfig['ssl_key_file']);
-                }
 
                 // 移除不需要的配置项及非swoole的自定义配置项
                 unset($portConfig['host'], $portConfig['port'], $portConfig['mode'], $portConfig['socketType'], $portConfig['cert']);
@@ -141,7 +132,7 @@ class Server
 
                 $first = $k;
             } else {
-                // 其他的作为主服务的端口监听
+                // 其他的端口作为主服务的端口监听
                 /**
                  * @var $ports Port
                  */
@@ -183,85 +174,76 @@ class Server
     }
 
     /**
-     * 结束服务
+     * 停止服务
      *
      * @author Bowen
      * @email bowen@jiuchet.com
      *
      * @return bool
-     * @lasttime: 2022/8/5 2:54 PM
+     * @lasttime: 2023/11/16 2:48 PM
      */
     public function stop(): bool
     {
         if ($pid = $this->getPid()) {
-            if ($this->onWorkerStop($this->_ms, false)) {
+            if ($this->onWorkerStop($this->_ms, null)) {
                 return Process::kill($pid, SIGTERM);
             } else {
-                echo '结束服务被终止' . PHP_EOL;
+                $this->Controller->stdout('结束服务被终止' . PHP_EOL, BaseConsole::FG_YELLOW);
                 return false;
             }
         } else {
-            echo '进程未启动，无需停止' . PHP_EOL;
+            $this->Controller->stdout('进程未启动，无需停止' . PHP_EOL, BaseConsole::FG_YELLOW);
             return false;
         }
     }
 
     /**
+     * 监听worker启动
      *
      * @author Bowen
      * @email bowen@jiuchet.com
      *
      * @param SwServer $server
-     * @param int|bool $workerId
-     * @return bool|mixed
+     * @param int|null $workerId
      * @lasttime: 2022/8/5 2:54 PM
      */
-    public function onWorkerStart(SwServer $server, $workerId)
+    public function onWorkerStart(SwServer $server, ?int $workerId = null)
     {
+        $this->Controller->stdout("Start Websocket Worker, Ports:" . json_encode($this->ports) . PHP_EOL, BaseConsole::FG_GREEN);
 
-        echo("服务开始运行, 监听" . json_encode($this->ports) . PHP_EOL);
+        $global                = Context::getGlobal('TCP');
+        $global['server']      = $server;
+        $global['workerIds'][] = $workerId;
+        $global['workerIds']   = array_unique($global['workerIds']);
+        $global['on']          = 'start';
+        $global['tables']      = $this->_tables;
 
-        Context::setGlobal('TCP', [
-            'server'   => $server,
-            'workerId' => $workerId,
-            'on'       => 'start',
-            'tables'   => $this->_tables
-        ]);
-
-        if ($this->onServer) {
-            if (method_exists($this->onServer, 'onWorkerStart')) {
-                return call_user_func_array([$this->onServer, 'onWorkerStart'], [$server, $workerId, $this]);
-            }
-        }
-
-        return true;
+        Context::setGlobal('TCP', $global);
     }
 
     /**
+     * 监听worker停止
      *
      * @author Bowen
      * @email bowen@jiuchet.com
      *
      * @param $server
-     * @param int|bool $workerId
+     * @param int|null $workerId
      * @return bool
-     * @lasttime: 2022/8/5 2:43 PM
+     * @lasttime: 2023/11/16 2:52 PM
      */
-    public function onWorkerStop($server, $workerId): bool
+    public function onWorkerStop($server, ?int $workerId): bool
     {
         $global = Context::getGlobal('TCP');
 
         $global['server'] = $server;
         $global['on']     = 'stop';
+        // 移除当前workerId
+        $global['workerIds'] = array_diff($global['workerIds'], [$workerId]);
 
         Context::setGlobal('TCP', $global);
 
-        echo '进程已经停止' . PHP_EOL;
-
-        // 如果接管了onWorkerStop，则执行
-        if (!empty($this->onServer) && method_exists($this->onServer, 'onWorkerStop')) {
-            return call_user_func_array([$this->onServer, 'onWorkerStop'], [$server, $workerId, $this]);
-        }
+        $this->Controller->stdout('The worker has been stopped' . PHP_EOL, BaseConsole::FG_RED);
 
         Context::delGlobal('TCP');
 
@@ -269,42 +251,35 @@ class Server
     }
 
     /**
-     * 有新的连接进入时，在 worker 进程中回调。
+     * 有新连接进入时，在 worker 进程中回调。
      *
      * @author Bowen
      * @email bowen@jiuchet.com
      *
      * @param SwServer $server
-     * @param int $fd
-     * @param int $reactorId
-     * @return mixed|void
-     * @lasttime: 2022/8/15 2:58 PM
+     * @param int $fd 连接的文件描述符
+     * @param int $reactorId 连接所在的 Reactor 线程 ID
+     * @return bool
+     * @lasttime: 2023/11/16 2:53 PM
      */
-    public function onConnect(SwServer $server, int $fd, int $reactorId)
+    public function onConnect(SwServer $server, int $fd, int $reactorId): bool
     {
-        echo 'server: Connect Success!' . PHP_EOL;
+        $this->Controller->stdout("Connect Success (fd: $fd)" . PHP_EOL . PHP_EOL, BaseConsole::FG_GREEN);
 
-        $_B     = (array)Context::get('_B');
-        $global = (array)Context::getGlobal('TCP');
+        $_B = (array)Context::get('_B');
 
-        $_B['TCP']['fd']     = $fd;
-        $_B['TCP']['server'] = $server;
-        $_B['TCP']['on']     = 'connect';
-        $_B['TCP']['global'] = $global;
+        // 初始化上下文变量
+        $_B['TCP']['server']    = $server;
+        $_B['TCP']['fd']        = $fd;
+        $_B['TCP']['reactorId'] = $reactorId;
+        $_B['TCP']['on']        = 'connect';
 
         Context::set('_B', $_B);
 
-        // 如果接管了onConnect，则不再执行后续操作
-        if ($this->onServer) {
-            if (method_exists($this->onServer, 'onConnect')) {
-                return call_user_func_array([$this->onServer, 'onConnect'], [$server, $fd, $reactorId]);
-            }
-        }
-
-        return $server->send($fd, stripslashes(json_encode([
-            'errcode' => 0,
+        return $server->send($fd, json_encode([
+            'errcode' => ErrCode::SUCCESS,
             'errmsg'  => 'connect success',
-        ], JSON_UNESCAPED_UNICODE)));
+        ], JSON_UNESCAPED_UNICODE));
     }
 
     /**
@@ -315,34 +290,40 @@ class Server
      *
      * @param SwServer $server
      * @param int $fd
-     * @param int $reactor_id
+     * @param int $reactorId
      * @param string $data
      * @return mixed|void
      * @throws InvalidRouteException
      * @lasttime: 2022/8/15 2:58 PM
      */
-    public function onReceive(SwServer $server, int $fd, int $reactor_id, string $data)
+    public function onReceive(SwServer $server, int $fd, int $reactorId, string $data)
     {
         $_B   = Context::get('_B');
         $_GPC = Context::get('_GPC');
 
-        // 信息丢失，请重新连接
-        if (empty($_B['TCP']['fd'])) return $server->send($fd, stripslashes(json_encode([
-            'errcode' => 1,
-            'errmsg'  => 'connect info lost',
-        ], JSON_UNESCAPED_UNICODE)));
+        // 如果全局变量中的fd不存在，就意味着数据丢失了，需要客户端重新发起连接
+        if (empty($_B['TCP']['fd'])) {
+            $server->send($fd, json_encode([
+                'errcode' => ErrCode::LOST_CONNECTION,
+                'errmsg'  => 'connect info lost',
+            ], JSON_UNESCAPED_UNICODE));
+            return $server->close($fd);
+        }
 
         // 修改上下文中的信息
-        $_B['TCP']['on']     = 'message';
-        $_B['TCP']['server'] = $server;
+        $_B['TCP']['on']        = 'receive';
+        $_B['TCP']['server']    = $server;
+        $_B['TCP']['reactorId'] = $reactorId;
 
         $jsonData = Util::isJson($data) ? (array)@json_decode($data, true) : $data;
+        $jsonData = $jsonData ?: $data; // 避免因json解析失败导致数据丢失的情况
 
         // 空数据为触发心跳
-        if (empty($jsonData)) return $server->send($fd, stripslashes(json_encode([
-            'errcode' => 0,
-            'errmsg'  => 'Heart Success'
-        ], JSON_UNESCAPED_UNICODE)));
+        if (empty($jsonData))
+            return $server->send($fd, json_encode([
+                'errcode' => ErrCode::SUCCESS,
+                'errmsg'  => 'Heart Success'
+            ], JSON_UNESCAPED_UNICODE));
 
         if (is_array($jsonData)) {
             $route = $cacheRoute = $_B['TCP']['params']['route'];
@@ -352,51 +333,40 @@ class Server
             $gpcRoute = trim($_GPC['route']);
             $route    = $gpcRoute ?: $route;
 
-            if (empty($route)) return $server->send($fd, stripslashes(json_encode([
-                'errcode' => 9002010,
-                'errmsg'  => 'Empty Route',
-                'cr'      => $cacheRoute,
-                'gr'      => $gpcRoute,
-            ], JSON_UNESCAPED_UNICODE)));
+            // 如果route不存在，不知道应该由哪个路由进行处理，只能进行报错处理
+            if (empty($route))
+                return $server->send($fd, json_encode([
+                    'errcode' => ErrCode::PARAMETER_ERROR,
+                    'errmsg'  => 'Empty Route',
+                    'cr'      => $cacheRoute,
+                    'gr'      => $gpcRoute,
+                ], JSON_UNESCAPED_UNICODE));
 
             $_B['TCP']['params']['route'] = $route;
 
+            // 更新上下文中的信息
             Context::set('_B', $_B);
             Context::set('_GPC', $_GPC);
-
-            // 如果接管了onMessage，则不再执行
-            if ($this->onServer) {
-                if (method_exists($this->onServer, 'onReceive')) {
-                    return call_user_func_array([$this->onServer, 'onReceive'], [$server, $fd, $reactor_id, $data]);
-                }
-            }
 
             // 根据json数据中的路由转发到控制器内进行处理
             try {
                 return Yii::$app->runAction($route);
             } catch (Exception $e) {
                 Yii::info($e);
-                echo($e->getMessage());
+                $this->Controller->stdout($e->getMessage() . PHP_EOL, BaseConsole::FG_RED);
                 return false;
             }
-        }else{
-            // 如果接管了onMessage，则不再执行
-            if ($this->onServer) {
-                if (method_exists($this->onServer, 'onReceive')) {
-                    return call_user_func_array([$this->onServer, 'onReceive'], [$server, $fd, $reactor_id, $data]);
-                }
-            }
-
+        } else {
             // 数据格式错误
-            return $server->send($fd, stripslashes(json_encode([
-                'errcode' => 9002015,
+            return $server->send($fd, json_encode([
+                'errcode' => ErrCode::ILLEGAL_FORMAT,
                 'errmsg'  => 'Data Format Error',
-            ], JSON_UNESCAPED_UNICODE)));
+            ], JSON_UNESCAPED_UNICODE));
         }
     }
 
     /**
-     * 监听关闭
+     * 监听关闭连接
      *
      * @author Bowen
      * @email bowen@jiuchet.com
@@ -405,25 +375,18 @@ class Server
      * @param $fd
      * @return false|int|mixed|Response
      * @throws InvalidRouteException
-     * @lasttime: 2022/8/5 2:39 PM
+     * @lasttime: 2023/11/16 3:13 PM
      */
     public function onClose($server, $fd)
     {
         $_B = Context::get('_B');
 
-        echo "client-$fd is closed" . PHP_EOL;
+        $this->Controller->stdout("client-$fd is closed" . PHP_EOL);
 
         $_B['TCP']['server'] = $server;
-        $_B['TCP']['frame']  = [];
         $_B['TCP']['on']     = 'close';
 
         Context::set('_B', $_B);
-
-        if ($this->onServer) {
-            if (method_exists($this->onServer, 'onClose')) {
-                return call_user_func_array([$this->onServer, 'onClose'], [$server, $fd, $this]);
-            }
-        }
 
         $route = $_B['TCP']['params']['route'];
         if (!empty($route) && $route != '/') {
@@ -431,33 +394,35 @@ class Server
                 return Yii::$app->runAction($route);
             } catch (Exception $e) {
                 Yii::info($e);
-                echo($e->getMessage());
+                $this->Controller->stdout($e->getMessage() . PHP_EOL, BaseConsole::FG_RED);
             }
         }
         return false;
     }
 
     /**
-     * 获取pid
+     * 获取进程id(PID)
      *
      * @author Bowen
      * @email bowen@jiuchet.com
      *
      * @return false|string
-     * @lasttime: 2022/8/5 2:39 PM
+     * @lasttime: 2023/11/16 3:42 PM
      */
     public function getPid()
     {
-        if (empty($this->pidFile)) return false;
+        if (empty($this->pidFile))
+            return false;
+
         $pid_file = Yii::getAlias($this->pidFile);
         if (file_exists($pid_file)) {
             $pid = file_get_contents($pid_file);
-            if (posix_getpgid($pid)) {
+            if (posix_getpgid($pid))
                 return $pid;
-            } else {
+            else
                 unlink($pid_file);
-            }
         }
+
         return false;
     }
 }
