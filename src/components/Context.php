b5365e959da9db46c467071b24e68eb1c2f6ef4d
jcbowen <bowen@jiuchet.com>
2023-11-17 10:25:38 +0800

    完善 上下文类

--- src/components/Context.php
+++ src/components/Context.php
@@ -1,103 +1,160 @@
 <?php
-/**
- * 上下文类
- * @descripton: 在 Context 类中，使用 Coroutine::getuid 获取了协程 ID，然后隔离不同协程之间的全局变量，协程退出时清理上下文数据。
- */
 
 namespace Jcbowen\yiiswoole\components;
 
 use Swoole\Coroutine;
+use Swoole\ExitException;
 
+/**
+ * Class Context
+ *
+ * @descripton 协程上下文数据，在 Context 类中，使用 Coroutine::getuid 获取了协程 ID，然后隔离不同协程之间的全局变量，协程退出时清理上下文数据。
+ * @author Bowen
+ * @email bowen@jiuchet.com
+ * @lasttime: 2023/11/14 4:29 PM
+ * @package Jcbowen\yiiswoole\components
+ */
 class Context
 {
-    protected static $pool = [];
-
-    protected static $globalPool = [];
+    /**
+     * @var array 会话上下文
+     */
+    protected static array $pool = [];
 
     /**
+     * @var array 全局上下文
+     */
+    protected static array $globalPool = [];
+
+    /**
+     * 获取会话数据
      *
      * @author Bowen
      * @email bowen@jiuchet.com
      *
-     * @param $key
-     * @return mixed|null
-     * @lasttime: 2022/8/5 9:05 AM
+     * @param string $key
+     * @return mixed
+     * @lasttime: 2023/11/14 4:17 PM
      */
-    static function get($key)
+    public static function get(string $key)
     {
         $cid = Coroutine::getuid();
-        if ($cid < 0) {
+        if ($cid < 1)
             return null;
-        }
-        if (isset(self::$pool[$cid][$key])) {
-            return self::$pool[$cid][$key];
-        }
-        return null;
+
+        return empty($key) ? static::$pool[$cid] : (self::$pool[$cid][$key] ?? null);
     }
 
     /**
+     * 设置会话数据
      *
      * @author Bowen
      * @email bowen@jiuchet.com
      *
-     * @param $key
-     * @param $item
-     * @lasttime: 2022/8/5 9:06 AM
+     * @param string $key
+     * @param mixed $content
+     * @return bool
+     * @lasttime: 2023/11/14 4:19 PM
      */
-    static function set($key, $item)
+    public static function set(string $key, $content): bool
     {
         $cid = Coroutine::getuid();
-        if ($cid > 0) {
-            self::$pool[$cid][$key] = $item;
-        }
+        if ($cid < 1)
+            return false;
+
+        if (empty($key))
+            self::$pool[$cid] = $content;
+        else
+            self::$pool[$cid][$key] = $content;
+
+        return true;
     }
 
     /**
+     * 清理会话数据
      *
      * @author Bowen
      * @email bowen@jiuchet.com
      *
-     * @param $key
-     * @lasttime: 2022/8/5 9:06 AM
+     * @param string|null $key
+     * @lasttime: 2023/11/14 4:33 PM
      */
-    static function del($key = null)
+    public static function del(string $key = null)
     {
         $cid = Coroutine::getuid();
-        if ($cid > 0) {
-            if ($key == null) {
+        if ($cid > 0)
+            if ($key == null)
                 unset(self::$pool[$cid]);
-            } elseif (isset(self::$pool[$cid][$key])) {
+            elseif (isset(self::$pool[$cid][$key]))
                 unset(self::$pool[$cid][$key]);
-            }
-        }
     }
 
     //--- 未隔离，Begin ---/
-    static function getGlobal($key)
+
+    /**
+     * 获取全局会话数据
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param string|null $key
+     * @return mixed
+     * @lasttime: 2023/11/14 4:33 PM
+     */
+    public static function getGlobal(string $key = null)
     {
-        return self::$globalPool[$key];
+        return $key == null ? self::$globalPool : self::$globalPool[$key];
     }
 
-    static function setGlobal($key, $item)
+    /**
+     * 设置全局会话数据
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param string|null $key
+     * @param mixed $content
+     * @lasttime: 2023/11/14 4:34 PM
+     */
+    public static function setGlobal(string $key, $content)
     {
-        self::$globalPool[$key] = $item;
+        if ($key == null)
+            self::$globalPool = $content;
+        else
+            self::$globalPool[$key] = $content;
     }
 
-    static function delGlobal($key = null)
+    /**
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param string|null $key
+     * @lasttime: 2023/11/14 4:36 PM
+     */
+    public static function delGlobal(string $key = null)
     {
-        if ($key == null) {
+        if ($key == null)
             self::$globalPool = [];
-        } elseif (isset(self::$globalPool[$key])) {
+        elseif (isset(self::$globalPool[$key]))
             unset(self::$globalPool[$key]);
-        }
     }
+
     //--- 未隔离，End ---/
 
-    static function swooleExit()
+    /**
+     * 退出swoole
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @lasttime: 2023/11/14 4:37 PM
+     */
+    public static function swooleExit()
     {
         try {
             exit(0);
-        } catch (\Swoole\ExitException $e) {
+        } catch (ExitException $e) {
             echo $e->getMessage() . PHP_EOL;
         }
     }
